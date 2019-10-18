8a79a142def279ffb4d29105f39a717ce8dcfbd5
Miguel Valero <miguel.valero@upc.edu>
2019-10-18 11:58:37 +0200

    Ha añadido recomendaciones estilos

---
+++ src/app/estilos/estilos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estilos',
+  templateUrl: './estilos.component.html',
+  styleUrls: ['./estilos.component.scss']
+})
+export class EstilosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
