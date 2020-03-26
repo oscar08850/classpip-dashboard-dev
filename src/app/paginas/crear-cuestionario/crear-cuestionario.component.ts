54b61e3cd9c1026c34fc1719f648a970ad9e44fe
Ruben <rubencobe96@gmail.com>
2020-03-26 18:39:27 +0100

    Htmls funcionando de crear pregunta y crear cuestionario

--- src/app/paginas/crear-cuestionario/crear-cuestionario.component.ts
+++ src/app/paginas/crear-cuestionario/crear-cuestionario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { SesionService, PeticionesAPIService } from 'src/app/servicios';
 
 @Component({
   selector: 'app-crear-cuestionario',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCuestionarioComponent implements OnInit {
 
-  constructor() { }
+  // Para el stepper
+  myForm: FormGroup;
+
+  // Para saber si el botón está habilitado o no
+  // tslint:disable-next-line:ban-types
+  isDisabled: Boolean = true;
+    
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              public dialog: MatDialog,
+              public sesion: SesionService,
+              public peticionesAPI: PeticionesAPIService,
+              private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.myForm = this._formBuilder.group({
+      tituloCuestionario: ['', Validators.required],
+      descripcionCuestionario: ['', Validators.required]
+    })
   }
 
+  // MIRO SI HAY ALGO SIMULTÁNEAMENTE EN EL NOMBRE Y LA DESCRIPCIÓN
+  Disabled() {
+    if (this.myForm.value.tituloCuestionario === '' || this.myForm.value.descripcionCuestionario === '') {
+      // Si alguno de los valores es igual a nada, entonces estará desactivado
+      this.isDisabled = true;
+    } else {
+      // Si ambos son diferentes a nulo, estará activado.
+      this.isDisabled = false;
+    }
+  }
 }
--- src/app/paginas/pregunta/pregunta.component.ts
+++ src/app/paginas/pregunta/pregunta.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material';
+import { SesionService, PeticionesAPIService } from 'src/app/servicios';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pregunta',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreguntaComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  myForm2: FormGroup;
+
+  // Para saber si el botón está habilitado o no
+  // tslint:disable-next-line:ban-types
+  isDisabled: Boolean = true;
+  isDisabled2: Boolean = true;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              public dialog: MatDialog, 
+              public sesion: SesionService,
+              public peticionesAPI: PeticionesAPIService,
+              private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.myForm = this._formBuilder.group({
+      tituloPregunta: ['', Validators.required],
+      preguntaPregunta: ['', Validators.required],
+      tematicaPregunta: ['', Validators.required]
+    })
+
+    this.myForm2 = this._formBuilder.group({
+      respuestaCorrecta: ['', Validators.required],
+      respuestaIncorrecta1: ['', Validators.required],
+      respuestaIncorrecta2: ['', Validators.required],
+      respuestaIncorrecta3: ['', Validators.required],
+      feedbackCorrecto: ['', Validators.required],
+      feedbackIncorrecto: ['', Validators.required]
+    })
   }
 
+  // MIRO SI HAY ALGO SIMULTÁNEAMENTE EN EL TITULO, PREGUNTA Y LA TEMATICA
+  Disabled() {
+    if (this.myForm.value.tituloPregunta === '' || this.myForm.value.preguntaPregunta === '' || this.myForm.value.tematicaPregunta === '') {
+      // Si alguno de los valores es igual a nada, entonces estará desactivado
+      this.isDisabled = true;
+    } else {
+      // Si ambos son diferentes a nulo, estará activado.
+      this.isDisabled = false;
+    }
+  }
+
+  // MIRO SI HAY ALGO SIMULTÁNEAMENTE EN EL TODAS LAS RESPUESTAS Y EN LOS FEEDBACKS
+  Disabled2() {
+    if (this.myForm2.value.respuestaCorrecta === '' || this.myForm2.value.respuestaIncorrecta1 === '' ||
+     this.myForm2.value.respuestaIncorrecta2 === '' || this.myForm2.value.respuestaIncorrecta3 === '' ||
+      this.myForm2.value.feedbackCorrecto === '' || this.myForm2.value.feedbackIncorrecto === '') {
+      // Si alguno de los valores es igual a nada, entonces estará desactivado
+      this.isDisabled2 = true;
+    } else {
+      // Si ambos son diferentes a nulo, estará activado.
+      this.isDisabled2 = false;
+    }
+  }
 }
