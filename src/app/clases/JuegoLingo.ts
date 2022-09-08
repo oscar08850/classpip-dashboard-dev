269a5d2e28ebc8785f97354f928ebd6e54126d40
oscar08850 <oscar08850@gmail.com>
2022-09-08 20:02:13 +0200

    fixes

--- src/app/clases/JuegoLingo.ts
+++ src/app/clases/JuegoLingo.ts
@@ -1,31 +1,36 @@
-export class JuegoLINGO 
-{ //Juego de evaluacion
-    id: number;
-    NombreJuego: string;
-    Palabra: string;
-    Tipo: string;
-    Modo: string;
-    JuegoActivo: boolean;
-    JuegoTerminado: boolean;
-    letras: number;
-    intentos: number;
-    profesorId: number;
-    grupoId: number;
-  
-    // tslint:disable-next-line:max-line-length
-    //constructor(Tipo: string, NombreJuego: string, Palabra: string,  Modo: string, JuegoActivo: boolean, JuegoTerminado: boolean, letras : number, intentos: number, profesorId: number, grupoId: number) {
-      constructor(Tipo: string, Modo: string, Palabra: string, JuegoActivo: boolean, letras : number, intentos: number) {
-      
-      //this.NombreJuego = NombreJuego;
-      this.Palabra = Palabra;
-      this.Tipo = Tipo;
-      this.Modo = Modo;
-      this.JuegoActivo = JuegoActivo;
-      this.letras = letras;
-      this.intentos = intentos;
-      //this.profesorId = profesorId;
-      //this.grupoId = grupoId;
-  
-    }
+export class JuegoLINGO {
+  //Juego de evaluacion
+  id: number;
+  NombreJuego: string;
+  palabraSecreta: string;
+  Tipo: string;
+  Modo: string;
+  JuegoActivo: boolean;
+  JuegoTerminado: boolean;
+  letras: number;
+  intentos: number;
+  profesorId: number;
+  grupoId: number;
+
+  // tslint:disable-next-line:max-line-length
+  //constructor(Tipo: string, NombreJuego: string, Palabra: string,  Modo: string, JuegoActivo: boolean, JuegoTerminado: boolean, letras : number, intentos: number, profesorId: number, grupoId: number) {
+  constructor(
+    Tipo: string,
+    Modo: string,
+    Palabra: string,
+    JuegoActivo: boolean,
+    letras: number,
+    intentos: number
+  ) {
+    this.NombreJuego = "TestNombreJuego";
+    this.palabraSecreta = Palabra;
+    this.Tipo = Tipo;
+    this.Modo = Modo;
+    this.JuegoActivo = JuegoActivo;
+    this.letras = letras;
+    this.intentos = intentos;
+    this.JuegoTerminado = true;
+     //this.profesorId = profesorId;
+    //this.grupoId = grupoId;
   }
-  
+}
